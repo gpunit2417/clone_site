32161e54550e3bc7985da2a8a2fc8a0b8a4e3f58
gpunit2417 <punitgoyal106@gmail.com>
2024-10-02 11:50:10 +0530

    Booking Website Clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoQuestion } from "react-icons/go";
+import { FaCarAlt } from "react-icons/fa";
+import { IoBedOutline } from "react-icons/io5";
+import { SlCalender } from "react-icons/sl";
+import { GoPeople } from "react-icons/go";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ margin: '0', padding: '0', top: '0' }}>
+      <div className="header" style={{ backgroundColor: 'rgb(0,62,255)', color: 'white', height: '60vh' }}>
+        <div style={{ display: 'flex', margin: '0 120px' }}>
+          <h2 style={{ marginRight: '450px' }}>Booking.com</h2>
+          <h2>INR</h2>
+          <GoQuestion style={{ margin: '24px', fontSize: '1.5rem' }} />
+          <h3 style={{ marginTop: '24px' }}>List your Property</h3>
+          <button style={{ height: '32px', margin: '20px', borderRadius: '.2rem', border: 'rgb(0,62,255)', color: 'blue', width: '75px' }}>Register</button>
+          <button style={{ height: '32px', margin: '20px -10px', borderRadius: '.2rem', border: 'rgb(0,62,255)', color: 'blue', width: '68px' }}>Sign In</button>
+        </div>
+
+
+        <div style={{ display: 'flex' }}>
+          <div style={{ display: 'flex', border: '1px solid white', borderRadius: '2rem', marginLeft: '120px' }}>
+            <IoBedOutline style={{ margin: '18px 0 13px 10px' }} />
+            <h3 style={{ margin: '13px 13px 13px 2px', fontWeight: '100' }}>Stays</h3>
+          </div>
+          <h3 style={{ margin: '13px', fontWeight: '100' }}>Flights</h3>
+          <h3 style={{ margin: '13px', fontWeight: '100' }}>Flight + Hotel</h3>
+          <div style={{ display: 'flex' }}>
+            <FaCarAlt style={{ margin: '18px 0 13px 10px', fontWeight: '100' }} />
+            <h3 style={{ margin: '13px 13px 13px 2px', fontWeight: '100' }}>Car rentals</h3>
+          </div>
+          <h3 style={{ margin: '13px', fontWeight: '100' }}>Attractions</h3>
+          <h3 style={{ margin: '13px', fontWeight: '100' }}>Airport taxis</h3>
+        </div>
+
+
+        <div style={{ margin: '80px 0 0 120px' }}>
+          <p style={{ fontSize: '50px', fontWeight: '700' }}>Find Your Next stay</p>
+          <h2 style={{ fontWeight: '400', marginTop: '-50px' }}>Search low prices on hotels, homes and much more...</h2>
+        </div>
+      </div>
+
+
+      <div>
+        <div style={{ display: 'flex', border: '1px solid yellow', width: '1050px', margin: '-40px 0 0 150px', backgroundColor: 'white' }}>
+          <div style={{ border: '1px solid yellow', width: '350px', display: 'flex' }}>
+            <IoBedOutline style={{ margin: '23px 0 13px 10px' }} />
+            <h5 style={{ marginLeft: '2px' }}>Where are you going?</h5>
+          </div>
+          
+          <div style={{ border: '1px solid yellow', width: '350px', display: 'flex' }}>
+            <SlCalender style={{ margin: '23px 0 13px 10px' }} />
+            <h5 style={{ marginLeft: '2px' }}>Check-in date - Check-out date</h5>
+          </div>
+          <div style={{ border: '1px solid yellow', width: '350px', display: 'flex' }}>
+            <GoPeople style={{ margin: '23px 0 13px 10px' }} />
+            <h5 style={{ marginLeft: '2px' }}>2 adults. 0 children. 1 room</h5>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
