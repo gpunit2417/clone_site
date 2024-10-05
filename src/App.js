801caf31395f60456e7dd74b202d523a9a744758
gpunit2417 <punitgoyal106@gmail.com>
2024-10-05 21:50:31 +0530

    modified offer section

--- src/App.js
+++ src/App.js
@@ -77,15 +77,17 @@ function App() {
           <p style={{ fontSize: '20px', margin: '-15px auto auto auto' }}>Promotions, Deals and special offers for you</p>
         </div>
         <div style={{ display: 'flex', width: '90%' }}>
-          <div style={{ width: '50%', border: '1px solid grey', margin: '20px 5px 10px 0', borderRadius: '.4rem' }}>
+          <div style={{ width: '50%', border: '1px solid grey', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px'}}>
             <h3>Fly away to your dream holiday</h3>
             <p>Get inspired, compare and book flights with more flexibility</p>
-            <button>Search for flights</button>
+            <img src='https://q-xx.bstatic.com/xdata/images/xphoto/500x500/184698944.png?k=6bb1bf3c13db4a7ba3c22a2d1f1051f793c525a78104703b4dec3eb12101f545&o=' alt='logo' style={{width: '100px', float: 'right', margin: '-80px 2px 0 auto'}} />
+            <button style={{ height: '35px', backgroundColor: 'rgb(0,62,255)', color: 'white', border: 'rgb(0,62,255)', borderRadius: '.3rem'}}>Search for flights</button>
           </div>
-          <div style={{ width: '50%', border: '1px solid grey', margin: '20px 0 10px 5px', borderRadius: '.4rem' }}>
+          <div style={{ width: '50%', border: '1px solid grey', margin: '20px 0 10px 5px', borderRadius: '.4rem', padding: '5px 7px'}}>
             <h3>Go for a good time, not a long time</h3>
-            <p>Finish your year with a mini break. Save 15% or more when you book and stay by 7 January 2025.</p>
-            <button>Find late escape deals</button>
+            <p>Finish your year with a mini break. Save 15% or more <br/>when you book and stay by 7 January 2025.</p>
+            <img src='https://q-xx.bstatic.com/xdata/images/xphoto/500x500/372051085.jpeg?k=f5182852fa2c998e2b47cfd922da41ae16c42beb02fb70ecb57ee55ced3271f6&o=' alt='logo' style={{width: '130px', float: 'right', margin: '-108px 10px auto auto'}} />
+            <button style={{ height: '35px', backgroundColor: 'rgb(0,62,255)', color: 'white', border: 'rgb(0,62,255)', borderRadius: '.3rem'}}>Find late escape deals</button>
           </div>
         </div>
       </div>
