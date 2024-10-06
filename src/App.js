bc83577cc549aafd12ec18357380e3e227ac463d
gpunit2417 <punitgoyal106@gmail.com>
2024-10-06 20:57:57 +0530

    added images in trending destination section

--- src/App.js
+++ src/App.js
@@ -134,15 +134,28 @@ function App() {
       </div>
 
       <div className="trending">
-        <div></div>
         <div>
-          <div></div>
-          <div></div>
+          <h1>Trending Destinations</h1>
+          <p>Most popular choices for travellers from India</p>
         </div>
         <div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>
+            <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684765.jpg?k=3f7d20034c13ac7686520ac1ccf1621337a1e59860abfd9cbd96f8d66b4fc138&o=" alt="trending" />
+          </div>
+          <div>
+            <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684534.jpg?k=d1fe86c22f2433f4e2dda14ddcbe80feb024b0fb30305e5684a1241fba5d4cff&o=" alt="location" />
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="https://cf2.bstatic.com/xdata/images/city/600x600/971346.jpg?k=40eeb583a755f2835f4dcb6900cdeba2a46dc9d50e64f2aa04206f5f6fce5671&o=" alt="trending" />
+          </div>
+          <div>
+            <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684730.jpg?k=e37b93d88c1fe12e827f10c9d6909a1def7349be2c68df5de885deaa4bc01ee3&o=" alt="trending" />
+          </div>
+          <div>
+            <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684653.jpg?k=306ccafcc8a4a7e23b9e8a05b183453fe885b312a4daa5ce76ec39a1b79cbc6f&o=" alt="location" />
+          </div>
         </div>
       </div>
     </div>
