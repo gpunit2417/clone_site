bd195884cb2076ef00ee0dfa652e46a56b97e400
gpunit2417 <punitgoyal106@gmail.com>
2024-10-07 19:15:50 +0530

    added indian flag on top

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ import { GoPeople } from "react-icons/go";
 function App() {
   return (
     <div className="App" style={{ margin: '0', padding: '0', top: '0' }}>
-      <div className="header" style={{ backgroundColor: 'rgb(0,62,255)', color: 'white', height: '60vh' }}>
+      <div className="header" style={{ backgroundColor: 'rgb(0,60,145)', color: 'white', height: '60vh' }}>
         <div style={{ display: 'flex', margin: '0 120px' }}>
           <h2 style={{ marginRight: '450px' }}>Booking.com</h2>
           <h2>INR</h2>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1J4X8LJ0h28DpeUykXQcMiNAXXWLhvhW0g&s" alt="flag" style={{height: '25px', width: '25px', margin: '26px 0 0 15px', borderRadius: '1rem', objectFit: 'cover'}}/>
           <GoQuestion style={{ margin: '24px', fontSize: '1.5rem' }} />
           <h3 style={{ marginTop: '24px' }}>List your Property</h3>
           <button style={{ height: '32px', margin: '20px', borderRadius: '.2rem', border: 'rgb(0,62,255)', color: 'blue', width: '75px' }}>Register</button>
@@ -135,26 +136,26 @@ function App() {
         </div>
         <div style={{ display: 'flex', width: '90%' }}>
           <div style={{ width: '50%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
-          <h2 style={{top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white'}}>New Delhi</h2>
+          <h2 style={{top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white', textShadow: ' 1px 1px black'}}>New Delhi</h2>
             <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684765.jpg?k=3f7d20034c13ac7686520ac1ccf1621337a1e59860abfd9cbd96f8d66b4fc138&o=" alt="trending" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover',height: '50vh' }}/>
           </div>
           <div style={{ width: '50%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
-          <h2 style={{top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white'}}>Bangalore</h2>
-            <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684534.jpg?k=d1fe86c22f2433f4e2dda14ddcbe80feb024b0fb30305e5684a1241fba5d4cff&o=" alt="location" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover',height: '50vh' }}/>
+          <h2 style={{top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white', textShadow: ' 1px 1px black'}}>Bangalore</h2>
+            <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684534.jpg?k=d1fe86c22f2433f4e2dda14ddcbe80feb024b0fb30305e5684a1241fba5d4cff&o=" alt="location" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover',height: '50vh'}}/>
           </div>
         </div>
 
         <div style={{ display: 'flex', width: '90%', margin: '-15px 0 0 0' }}>
           <div style={{ width: '33%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
-          <h2 style={{top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white'}}>Mumbai</h2>
+          <h2 style={{top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white', textShadow: ' 1px 1px black'}}>Mumbai</h2>
             <img src="https://cf2.bstatic.com/xdata/images/city/600x600/971346.jpg?k=40eeb583a755f2835f4dcb6900cdeba2a46dc9d50e64f2aa04206f5f6fce5671&o=" alt="trending" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover',height: '40vh' }}/>
           </div>
           <div style={{ width: '33%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
-          <h2 style={{top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white'}}>Chennai</h2>
+          <h2 style={{top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white', textShadow: ' 1px 1px black'}}>Chennai</h2>
             <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684730.jpg?k=e37b93d88c1fe12e827f10c9d6909a1def7349be2c68df5de885deaa4bc01ee3&o=" alt="trending" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover',height: '40vh' }}/>
           </div>
           <div style={{ width: '33%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
-          <h2 style={{top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white'}}>Hyderabad</h2>
+          <h2 style={{top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white', textShadow: ' 1px 1px black'}}>Hyderabad</h2>
             <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684653.jpg?k=306ccafcc8a4a7e23b9e8a05b183453fe885b312a4daa5ce76ec39a1b79cbc6f&o=" alt="location" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover', height: '40vh' }}/>
           </div>
         </div>
