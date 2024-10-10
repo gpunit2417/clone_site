5464ec40401de0e8caf1fa2063c8378a29dbab1e
gpunit2417 <punitgoyal106@gmail.com>
2024-10-10 17:27:54 +0530

    quick section started

--- src/App.js
+++ src/App.js
@@ -198,6 +198,50 @@ function App() {
           </div>
         </div>
       </div>
+      <div className="quick" style={{ margin: '-10px auto 0 120px' }}>
+        <div>
+          <h1>Quick and easy trip planner</h1>
+          <p style={{ fontSize: '20px', margin: '-15px auto auto auto' }}>Pick a vibe and explore the top destinations in India</p>
+        </div>
+        <div></div>
+        <div style={{ display: 'flex', width: '90%' }}>
+        <div className="places" style={{ width: '50%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
+            <img src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140052521.jpg?k=a6bd9352c790d92b6969bcfbab4ec45793e60579c6212519ad6d4f6817e41219&o=" alt="location" style={{ borderRadius: '.5rem' }} />
+            <h4 style={{ margin: '5px 0px 0px 0px' }}>Nedumbassery</h4>
+            <p style={{ margin: '0px' }}>74 km from Munnar</p>
+          </div>
+
+          <div className="places" style={{ width: '50%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
+            <img src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018250.jpg?k=7b9d3f59146dead4c932675f7c1bd5f6f4f74390c5584d00320cc51d6593ba3a&o=" alt="location" style={{ borderRadius: '.5rem' }} />
+            <h4 style={{ margin: '5px 0px 0px 0px' }}>Kottayam</h4>
+            <p style={{ margin: '0px' }}>81 km from Munnar</p>
+          </div>
+
+          <div className="places" style={{ width: '50%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
+            <img src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018293.jpg?k=eba1b4abc1a196e422d559e205efd895999a9f5e643bc09544d91f9d18b30733&o=" alt="location" style={{ borderRadius: '.5rem' }} />
+            <h4 style={{ margin: '5px 0px 0px 0px' }}>Ernakulam</h4>
+            <p style={{ margin: '0px' }}>86 km from Munnar</p>
+          </div>
+
+          <div className="places" style={{ width: '50%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
+            <img src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140052552.jpg?k=478b1b6119830c85ba58e1509b36890c1c9083974446f9f132aedbbed0af877d&o=" alt="location" style={{ borderRadius: '.5rem' }} />
+            <h4 style={{ margin: '5px 0px 0px 0px' }}>Ambalavayal</h4>
+            <p style={{ margin: '0px' }}>194 km from Munnar</p>
+          </div>
+
+          <div className="places" style={{ width: '50%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
+            <img src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018163.jpg?k=f36cf9c92731c02afead1f697dd091271ac62ad0d1d904ad937d571b2f6a17ae&o=" alt="location" style={{ borderRadius: '.5rem' }} />
+            <h4 style={{ margin: '5px 0px 0px 0px' }}>Sultan Bathery</h4>
+            <p style={{ margin: '0px' }}>195 km from Munnar</p>
+          </div>
+          
+          <div className="places" style={{ width: '50%', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
+            <img src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018181.jpg?k=8701d90839626e2b1dedfbe09dd993366c383a71b9786d3677ed8ebc61bd64a9&o=" alt="location" style={{ borderRadius: '.5rem' }} />
+            <h4 style={{ margin: '5px 0px 0px 0px' }}>Salem</h4>
+            <p style={{ margin: '0px' }}>212 km from Munnar</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
