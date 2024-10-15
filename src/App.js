2b720a78e86e30f757eb52f749bedd24ceb04a96
gpunit2417 <punitgoyal106@gmail.com>
2024-10-15 21:15:01 +0530

    new section started

--- src/App.js
+++ src/App.js
@@ -515,6 +515,58 @@ function App() {
           Reviews.
           Discover monthly stays</p>
       </div>
+      <div className='footer' style={{ margin: '-10px auto 0 120px', width: '80%' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            <h4 style={{ marginBottom: '-10px' }}>Lake District</h4>
+            <p>2,461 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Phuket Province</h4>
+            <p>5,547 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Zanzibar</h4>
+            <p>882 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Texel</h4>
+            <p>411 properties</p>
+          </div>
+          <div>
+            <h4 style={{ marginBottom: '-10px' }}>Hawai</h4>
+            <p>5,452 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Bora Bora</h4>
+            <p>69 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Bihar</h4>
+            <p>781 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>England</h4>
+            <p>80,602 properties</p>
+          </div>
+          <div>
+            <h4 style={{ marginBottom: '-10px' }}>Mykonos</h4>
+            <p>1,455 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Bali</h4>
+            <p>13,206 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Guernsey</h4>
+            <p>64 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Cornwall</h4>
+            <p>5,459 properties</p>
+          </div>
+          <div>
+            <h4 style={{ marginBottom: '-10px' }}>Uttar Pradesh</h4>
+            <p>4,463 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Santorini</h4>
+            <p>1,794 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Jersey</h4>
+            <p>90 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Tenerife</h4>
+            <p>9,735 properties</p>
+          </div>
+          <div>
+            <h4 style={{ marginBottom: '-10px' }}>Ras Al Khaimah</h4>
+            <p>117 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Ibiza</h4>
+            <p>1,642 properties</p>
+            <h4 style={{ marginBottom: '-10px' }}>Isle of Wight</h4>
+            <p>961 properties</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
