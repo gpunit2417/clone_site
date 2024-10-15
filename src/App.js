6115230290c1a8109e03c6bfd130b6a723b28dbe
gpunit2417 <punitgoyal106@gmail.com>
2024-10-15 17:21:58 +0530

    one more section complete

--- src/App.js
+++ src/App.js
@@ -490,29 +490,29 @@ function App() {
           </div>
         </div>
       </div>
-      <div style={{ margin: '-10px auto 0 120px', width: '80%' }}>
-        <p>Countries
-          Regions
-          Cities
-          Districts
-          Airports
-          Hotels
-          Places of interest
-          Holiday Homes
-          Apartments
-          Resorts
-          Villas
-          Hostels
-          B&Bs
-          Guest Houses
-          Unique places to stay
-          All destinations
-          All flight destinations
-          All car hire locations
-          All holiday destinations
-          Guides
-          Discover
-          Reviews
+      <div style={{ margin: '40px auto 0 120px', width: '80%' }}>
+        <p style={{fontSize: '13.3px'}}>Countries.
+          Regions.
+          Cities.
+          Districts.
+          Airports.
+          Hotels.
+          Places of interest.
+          Holiday Homes.
+          Apartments.
+          Resorts.
+          Villas.
+          Hostels.
+          B&Bs.
+          Guest Houses.
+          Unique places to stay.
+          All destinations.<br/><br/>
+          All flight destinations.
+          All car hire locations.
+          All holiday destinations.
+          Guides.
+          Discover.
+          Reviews.
           Discover monthly stays</p>
       </div>
     </div>
