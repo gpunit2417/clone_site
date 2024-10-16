8003db06421d57ed4d3492b151e86d183c31793c
gpunit2417 <punitgoyal106@gmail.com>
2024-10-16 21:10:07 +0530

    completed this clone

--- src/App.js
+++ src/App.js
@@ -493,97 +493,97 @@ function App() {
         </div>
       </div>
       <div style={{ margin: '40px auto 0 120px', width: '80%' }}>
-        <p style={{ fontSize: '13.3px' }}>Countries.
-          Regions.
-          Cities.
-          Districts.
-          Airports.
-          Hotels.
-          Places of interest.
-          Holiday Homes.
-          Apartments.
-          Resorts.
-          Villas.
-          Hostels.
-          B&Bs.
-          Guest Houses.
-          Unique places to stay.
-          All destinations.<br /><br />
-          All flight destinations.
-          All car hire locations.
-          All holiday destinations.
-          Guides.
-          Discover.
-          Reviews.
+        <p style={{ fontSize: '12px' }}>Countries .
+          Regions .
+          Cities .
+          Districts .
+          Airports .
+          Hotels .
+          Places of interest .
+          Holiday Homes .
+          Apartments .
+          Resorts .
+          Villas .
+          Hostels .
+          B&Bs .
+          Guest Houses .
+          Unique places to stay .
+          All destinations .<br /><br />
+          All flight destinations .
+          All car hire locations .
+          All holiday destinations .
+          Guides .
+          Discover .
+          Reviews .
           Discover monthly stays</p>
       </div>
-      <div style={{ backgroundColor: 'lightgrey' }}>
+      <div style={{ backgroundColor: 'rgb(245,245,245)' }}>
         <div className='footer' style={{ margin: '-1px auto 0 120px', width: '80%' }}>
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <div>
-              <h4 style={{ marginBottom: '-10px' }}>Support</h4>
-              <p>Coronavirus (COVID-19) FAQs</p>
-              <p>Manage your trips</p>
-              <p>Contact customer service</p>
-              <p>Safety resource center</p>
+              <h4 style={{ marginBottom: '-10px', fontSize: '14px' }}>Support</h4>
+              <p style={{ fontSize: '13.5px' }}>Coronavirus (COVID-19) FAQs</p>
+              <p style={{ fontSize: '13.5px' }}>Manage your trips</p>
+              <p style={{ fontSize: '13.5px' }}>Contact customer service</p>
+              <p style={{ fontSize: '13.5px' }}>Safety resource center</p>
             </div>
             <div>
-              <h4 style={{ marginBottom: '-10px' }}>Discover</h4>
-              <p>Genius loyalty programme
+              <h4 style={{ marginBottom: '-10px', fontSize: '14px' }}>Discover</h4>
+              <p style={{ fontSize: '13.5px' }}>Genius loyalty programme
               </p>
-              <p>Seasonal and holiday deals
+              <p style={{ fontSize: '13.5px' }}>Seasonal and holiday deals
 
               </p>
-              <p>Travel articles
+              <p style={{ fontSize: '13.5px' }}>Travel articles
 
               </p>
-              <p>Booking.com for Business
+              <p style={{ fontSize: '13.5px' }}>Booking.com for Business
               </p>
-              <p>Traveller Review Awards</p>
-              <p>Car hire</p>
-              <p>Flight finder
+              <p style={{ fontSize: '13.5px' }}>Traveller Review Awards</p>
+              <p style={{ fontSize: '13.5px' }}>Car hire</p>
+              <p style={{ fontSize: '13.5px' }}>Flight finder
               </p>
-              <p>Restaurant reservations
+              <p style={{ fontSize: '13.5px' }}>Restaurant reservations
               </p>
-              <p>Booking.com for Travel Agents</p>
+              <p style={{ fontSize: '13.5px' }}>Booking.com for Travel Agents</p>
             </div>
             <div>
-              <h4 style={{ marginBottom: '-10px' }}>Mykonos</h4>
-              <p>Privacy & cookies
+              <h4 style={{ marginBottom: '-10px', fontSize: '14px' }}>Mykonos</h4>
+              <p style={{ fontSize: '13.5px' }}>Privacy & cookies
               </p>
-              <p>Terms and conditions
+              <p style={{ fontSize: '13.5px' }}>Terms and conditions
               </p>
-              <p>Grievance officer
+              <p style={{ fontSize: '13.5px' }}>Grievance officer
               </p>
-              <p>Modern Slavery Statement
+              <p style={{ fontSize: '13.5px' }}>Modern Slavery Statement
               </p>
-              <p>Human Rights Statement</p>
+              <p style={{ fontSize: '13.5px' }}>Human Rights Statement</p>
             </div>
             <div>
-              <h4 style={{ marginBottom: '-10px' }}>Uttar Pradesh</h4>
-              <p>Extranet login
+              <h4 style={{ marginBottom: '-10px', fontSize: '14px' }}>Uttar Pradesh</h4>
+              <p style={{ fontSize: '13.5px' }}>Extranet login
               </p>
-              <p>Partner help
+              <p style={{ fontSize: '13.5px' }}>Partner help
               </p>
-              <p>List your property
+              <p style={{ fontSize: '13.5px' }}>List your property
               </p>
-              <p>Become an affiliate</p>
+              <p style={{ fontSize: '13.5px' }}>Become an affiliate</p>
             </div>
             <div>
-              <h4 style={{ marginBottom: '-10px' }}>Ras Al Khaimah</h4>
-              <p>About Booking.com
+              <h4 style={{ marginBottom: '-10px', fontSize: '14px' }}>Ras Al Khaimah</h4>
+              <p style={{ fontSize: '13.5px' }}>About Booking.com
               </p>
-              <p>How we work
+              <p style={{ fontSize: '13.5px' }}>How we work
               </p>
-              <p>Sustainability
+              <p style={{ fontSize: '13.5px' }}>Sustainability
               </p>
-              <p>Press centre
+              <p style={{ fontSize: '13.5px' }}>Press centre
               </p>
-              <p>Careers
+              <p style={{ fontSize: '13.5px' }}>Careers
               </p>
-              <p>Investor relations
+              <p style={{ fontSize: '13.5px' }}>Investor relations
               </p>
-              <p>Corporate contact</p>
+              <p style={{ fontSize: '13.5px' }}>Corporate contact</p>
             </div>
           </div>
         </div>
@@ -591,51 +591,52 @@ function App() {
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1J4X8LJ0h28DpeUykXQcMiNAXXWLhvhW0g&s" alt="flag" style={{ height: '25px', width: '25px', margin: '10px 40px 0 15px', borderRadius: '1rem', objectFit: 'cover' }} />
           <h4 style={{ marginTop: '10px' }}>INR</h4>
         </div>
+        <hr style={{ width: '80%', backgroundColor: 'lightgrey', border: 'none', height: '1px'}}/>
         <div>
-          <div style={{ textAlign: 'center' }}>
+          <div style={{ textAlign: 'center', fontSize: '12px' }}>
             <p>Booking.com is part of Booking Holdings Inc., the world leader in online travel and related services.<br />
               Copyright © 1996–2024 Booking.com™. All rights reserved.</p>
           </div>
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <div style={{margin: '0 5px 0 5px'}}>
-              <h3 style={{ color: 'rgb(0,60,145)' }}>Booking.com</h3>
+          <div style={{ display: 'flex', justifyContent: 'center', margin: '-15px 0' }}>
+            <div style={{ margin: '0 5px 0 5px' }}>
+              <h4 style={{ color: 'rgb(0,60,145)' }}>Booking.com</h4>
             </div>
-            <div style={{margin: '0 5px 0 5px'}}>
-              <h3 style={{ color: 'rgb(0,150,255)' }}>priceline<sup style={{ fontSize: '7px' }}>R</sup></h3>
+            <div style={{ margin: '0 5px 0 25px' }}>
+              <h4 style={{ color: 'rgb(0,150,255)' }}>priceline<sup style={{ fontSize: '7px' }}>R</sup></h4>
             </div>
-            <div style={{ display: 'flex', margin: '0 5px 0 5px' }}>
+            <div style={{ display: 'flex', margin: '2px 5px 0 25px' }}>
               <div style={{ margin: '0 1px' }}>
-                <h3 style={{ color: 'white', background: 'orange' }}>K</h3>
+                <h5 style={{ color: 'white', background: 'orange', width: '15px', height: '18px', textAlign: 'center' }}>K</h5>
               </div>
               <div style={{ margin: '0 1px' }}>
-                <h3 style={{ color: 'white', background: 'orange' }}>A</h3>
+                <h5 style={{ color: 'white', background: 'orange', width: '15px', height: '18px', textAlign: 'center' }}>A</h5>
               </div>
               <div style={{ margin: '0 1px' }}>
-                <h3 style={{ color: 'white', background: 'orange' }}>Y</h3>
+                <h5 style={{ color: 'white', background: 'orange', width: '15px', height: '18px', textAlign: 'center' }}>Y</h5>
               </div>
               <div style={{ margin: '0 1px' }}>
-                <h3 style={{ color: 'white', background: 'orange' }}>A</h3>
+                <h5 style={{ color: 'white', background: 'orange', width: '15px', height: '18px', textAlign: 'center' }}>A</h5>
               </div>
               <div style={{ margin: '0 1px' }}>
-                <h3 style={{ color: 'white', background: 'orange' }}>K</h3>
+                <h5 style={{ color: 'white', background: 'orange', width: '15px', height: '18px', textAlign: 'center' }}>K</h5>
               </div>
             </div>
-            <div style={{margin: '0 5px 0 5px'}}>
+            <div style={{ margin: '0 5px 0 25px' }}>
               <h5>agoda</h5>
               <div style={{ margin: '-30px 0 0 0' }}>
-                <FaCircle style={{ fontSize: '6px', color: 'red' }} />
-                <FaCircle style={{ fontSize: '6px', color: 'yellow' }} />
-                <FaCircle style={{ fontSize: '6px', color: 'green' }} />
-                <FaCircle style={{ fontSize: '6px', color: 'purple' }} />
-                <FaCircle style={{ fontSize: '6px', color: 'blue' }} />
+                <FaCircle style={{ fontSize: '6px', color: 'red', margin: '0 1px' }} />
+                <FaCircle style={{ fontSize: '6px', color: 'orange', margin: '0 1px' }} />
+                <FaCircle style={{ fontSize: '6px', color: 'green', margin: '0 1px' }} />
+                <FaCircle style={{ fontSize: '6px', color: 'purple', margin: '0 1px' }} />
+                <FaCircle style={{ fontSize: '6px', color: 'blue', margin: '0 1px' }} />
               </div>
             </div>
-            <div style={{ display: 'flex', margin: '0 5px 0 5px'}}>
-              <div style={{margin: '24px 0'}}>
-              <FaCircle style={{ fontSize: '4px', color: 'red' }} />
-              <FaDotCircle style={{ color: 'red' }} />
+            <div style={{ display: 'flex', margin: '0 5px 0 25px' }}>
+              <div style={{ margin: '22px 0' }}>
+                <FaCircle style={{ fontSize: '4px', color: 'red', margin: '-0 2px 5px 0' }} />
+                <FaDotCircle style={{ color: 'red', margin: '0 1px' }} />
               </div>
-              <h3>OpenTable</h3>
+              <h5>OpenTable</h5>
             </div>
           </div>
         </div>
