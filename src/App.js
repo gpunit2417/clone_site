e471f70eacd0c062e972eecab5dca68ea459ed94
gpunit2417 <punitgoyal106@gmail.com>
2024-10-17 12:36:18 +0530

    deals section divided

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Explore from './components/Explore';
 import Trending from './components/Trending';
 import Browse from './components/Browse';
 import Quick from './components/Quick';
+import Deals from './components/Deals';
 import { FaCircle } from "react-icons/fa";
 import { FaDotCircle } from "react-icons/fa";
 
@@ -19,87 +20,12 @@ function App() {
       <Trending/>
       <Browse/>
       <Quick/>
-      
+      <Deals/>
 
       
       
 
-      <div className="deals" style={{ margin: '-10px auto 0 120px' }}>
-        <div>
-          <h1>Deals for the weekend</h1>
-          <p style={{ fontSize: '20px', margin: '-15px auto auto auto' }}>Save on stays for 18 October - 20 October</p>
-        </div>
-
-        <div style={{ display: 'flex', justifyContent: 'space-around', width: '93%' }}>
-          <div className="places" style={{ width: '69%', margin: '20px 5px 10px 0', borderRadius: '.5rem', border: '1px grey', backgroundColor: 'rgb(250,250,250)' }}>
-            <img src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140052521.jpg?k=a6bd9352c790d92b6969bcfbab4ec45793e60579c6212519ad6d4f6817e41219&o=" alt="location" style={{ width: '280px', height: '207px', borderRadius: '.5rem .5rem .1rem .1rem' }} />
-            <p style={{ border: 'rgb(0, 60, 145)', width: '45px', fontSize: '12px', textAlign: 'center', backgroundColor: 'rgb(0, 60, 145)', color: '#fff', borderRadius: '.1rem', margin: '2px 4px' }}>Genius</p>
-            <h4 style={{ margin: '5px 0px 0px 2px' }}>Hotel Oasis Grand Near IGI Delhi Airport</h4>
-            <p style={{ margin: '0px 2px' }}>New Delhi, India</p>
-            <div style={{ display: 'flex', height: '30px' }}>
-              <p style={{ border: 'rgb(0, 60, 145)', width: '25px', fontSize: '12px', textAlign: 'center', backgroundColor: 'rgb(0, 60, 145)', color: '#fff', borderRadius: '.2rem', margin: '3px 4px', height: '30px', justifyContent: 'center' }}>8.1</p>
-              <p style={{ margin: 'auto 0' }}>Very good · 34 reviews</p>
-            </div>
-            <div style={{ margin: '30px 0 -2px 75px' }}>
-              <p>2 nights&nbsp;
-                <strike style={{ color: 'red' }}>₹ 2,752</strike>&nbsp;
-                <b>₹ 2,477</b>
-              </p>
-            </div>
-          </div>
-
-          <div className="places" style={{ width: '69%', margin: '20px 5px 10px 0', borderRadius: '.5rem', border: '1px grey', backgroundColor: 'rgb(250,250,250)' }}>
-            <img src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018250.jpg?k=7b9d3f59146dead4c932675f7c1bd5f6f4f74390c5584d00320cc51d6593ba3a&o=" alt="location" style={{ width: '280px', height: '207px', borderRadius: '.5rem .5rem .1rem .1rem' }} />
-            <p style={{ border: 'rgb(0, 60, 145)', width: '45px', fontSize: '12px', textAlign: 'center', backgroundColor: 'rgb(0, 60, 145)', color: '#fff', borderRadius: '.1rem', margin: '2px 4px' }}>Genius</p>
-            <h4 style={{ margin: '5px 0px 0px 2px' }}>Trim Boutique Parkota Haveli</h4>
-            <p style={{ margin: '0px 2px' }}>Jaipur, India</p>
-            <div style={{ display: 'flex', height: '30px' }}>
-              <p style={{ border: 'rgb(0, 60, 145)', width: '25px', fontSize: '12px', textAlign: 'center', backgroundColor: 'rgb(0, 60, 145)', color: '#fff', borderRadius: '.2rem', margin: '3px 4px', height: '30px', justifyContent: 'center' }}>8.1</p>
-              <p style={{ margin: 'auto 0' }}>Fabulous · 176 reviews</p>
-            </div>
-            <div style={{ margin: '30px 0 -2px 75px' }}>
-              <p>2 nights&nbsp;
-                <strike style={{ color: 'red' }}>₹ 8,998</strike>&nbsp;
-                <b>₹ 6,749</b>
-              </p>
-            </div>
-          </div>
-
-          <div className="places" style={{ width: '69%', margin: '20px 5px 10px 0', borderRadius: '.5rem', border: '1px grey', backgroundColor: 'rgb(250,250,250)' }}>
-            <img src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018293.jpg?k=eba1b4abc1a196e422d559e205efd895999a9f5e643bc09544d91f9d18b30733&o=" alt="location" style={{ width: '280px', height: '207px', borderRadius: '.5rem .5rem .1rem .1rem' }} />
-            <p style={{ border: 'rgb(0, 60, 145)', width: '45px', fontSize: '12px', textAlign: 'center', backgroundColor: 'rgb(0, 60, 145)', color: '#fff', borderRadius: '.1rem', margin: '2px 4px' }}>Genius</p>
-            <h4 style={{ margin: '5px 0px 0px 2px' }}>Limewood Stay - Executive Huda City Center</h4>
-            <p style={{ margin: '0px 2px' }}>Gurgaon, India</p>
-            <div style={{ display: 'flex', height: '30px' }}>
-              <p style={{ border: 'rgb(0, 60, 145)', width: '25px', fontSize: '12px', textAlign: 'center', backgroundColor: 'rgb(0, 60, 145)', color: '#fff', borderRadius: '.2rem', margin: '3px 4px', height: '30px', justifyContent: 'center' }}>8.1</p>
-              <p style={{ margin: 'auto 0' }}>Fabulous · 282 reviews</p>
-            </div>
-            <div style={{ margin: '30px 0 -2px 75px' }}>
-              <p>2 nights&nbsp;
-                <strike style={{ color: 'red' }}>₹ 6,804</strike>&nbsp;
-                <b>₹ 3,402</b>
-              </p>
-            </div>
-          </div>
-
-          <div className="places" style={{ width: '69%', margin: '20px 5px 10px 0', borderRadius: '.5rem', border: '1px grey', backgroundColor: 'rgb(250,250,250)' }}>
-            <img src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140052552.jpg?k=478b1b6119830c85ba58e1509b36890c1c9083974446f9f132aedbbed0af877d&o=" alt="location" style={{ width: '280px', height: '207px', borderRadius: '.5rem .5rem .1rem .1rem' }} />
-            <p style={{ border: 'rgb(0, 60, 145)', width: '45px', fontSize: '12px', textAlign: 'center', backgroundColor: 'rgb(0, 60, 145)', color: '#fff', borderRadius: '.1rem', margin: '2px 4px' }}>Genius</p>
-            <h4 style={{ margin: '5px 0px 0px 2px' }}>Hotel Luxury Villa Near Delhi Airport</h4>
-            <p style={{ margin: '0px 2px' }}>New Delhi, India</p>
-            <div style={{ display: 'flex', height: '30px' }}>
-              <p style={{ border: 'rgb(0, 60, 145)', width: '25px', fontSize: '12px', textAlign: 'center', backgroundColor: 'rgb(0, 60, 145)', color: '#fff', borderRadius: '.2rem', margin: '3px 4px', height: '30px', justifyContent: 'center' }}>8.1</p>
-              <p style={{ margin: 'auto 0' }}>Fabulous · 87 reviews</p>
-            </div>
-            <div style={{ margin: '30px 0 -2px 75px' }}>
-              <p>2 nights&nbsp;
-                <strike style={{ color: 'red' }}>₹ 3,076</strike>&nbsp;
-                <b>₹ 2,769</b>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      
       <div className='inspiration' style={{ margin: '-10px auto 0 120px', width: '80%' }}>
         <div style={{ display: 'flex' }}>
           <h1>Get inspiration for your next trip</h1>
