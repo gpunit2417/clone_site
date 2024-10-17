ebf1fa8ddec9e780a32259fb2e7079c741a77b13
gpunit2417 <punitgoyal106@gmail.com>
2024-10-17 12:26:23 +0530

    created a navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { GoQuestion } from "react-icons/go";
+import { FaCarAlt } from "react-icons/fa";
+import { IoBedOutline } from "react-icons/io5";
+import { SlCalender } from "react-icons/sl";
+import { GoPeople } from "react-icons/go";
+
+function Navbar() {
+  return (
+    <div>
+        <div className="header" style={{ backgroundColor: 'rgb(0,60,145)', color: 'white', height: '60vh' }}>
+        <div style={{ display: 'flex', margin: '0 120px' }}>
+          <h2 style={{ marginRight: '450px' }}>Booking.com</h2>
+          <h2>INR</h2>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1J4X8LJ0h28DpeUykXQcMiNAXXWLhvhW0g&s" alt="flag" style={{ height: '25px', width: '25px', margin: '26px 0 0 15px', borderRadius: '1rem', objectFit: 'cover' }} />
+          <GoQuestion style={{ margin: '24px', fontSize: '1.5rem' }} />
+          <h3 style={{ marginTop: '24px' }}>List your Property</h3>
+          <button style={{ height: '32px', margin: '20px', borderRadius: '.2rem', border: 'rgb(0,62,255)', color: 'blue', width: '75px' }}>Register</button>
+          <button style={{ height: '32px', margin: '20px -10px', borderRadius: '.2rem', border: 'rgb(0,62,255)', color: 'blue', width: '68px' }}>Sign In</button>
+        </div>
+
+        <div style={{ display: 'flex' }}>
+          <div style={{ display: 'flex', border: '1px solid white', borderRadius: '2rem', marginLeft: '120px' }}>
+            <IoBedOutline style={{ margin: '18px 0 13px 10px' }} />
+            <h3 style={{ margin: '13px 13px 13px 2px', fontWeight: '100' }}>Stays</h3>
+          </div>
+          <h3 style={{ margin: '13px', fontWeight: '100' }}>Flights</h3>
+          <h3 style={{ margin: '13px', fontWeight: '100' }}>Flight + Hotel</h3>
+          <div style={{ display: 'flex' }}>
+            <FaCarAlt style={{ margin: '18px 0 13px 10px', fontWeight: '100' }} />
+            <h3 style={{ margin: '13px 13px 13px 2px', fontWeight: '100' }}>Car rentals</h3>
+          </div>
+          <h3 style={{ margin: '13px', fontWeight: '100' }}>Attractions</h3>
+          <h3 style={{ margin: '13px', fontWeight: '100' }}>Airport taxis</h3>
+        </div>
+
+        <div style={{ margin: '80px 0 0 120px' }}>
+          <p style={{ fontSize: '50px', fontWeight: '700' }}>Find Your Next stay</p>
+          <h2 style={{ fontWeight: '400', marginTop: '-50px' }}>Search low prices on hotels, homes and much more...</h2>
+        </div>
+      </div>
+
+      <div>
+        <div style={{ display: 'flex', border: '1px solid yellow', width: '1150px', margin: '-40px 0 0 120px', backgroundColor: 'white', borderRadius: '.3rem', borderWidth: '2px' }}>
+          <div style={{ border: '1px solid yellow', width: '335px', display: 'flex', borderRadius: '.3rem', borderWidth: '2px' }}>
+            <IoBedOutline style={{ margin: '23px 0 13px 10px' }} />
+            <h5 style={{ marginLeft: '2px' }}>Where are you going?</h5>
+          </div>
+
+          <div style={{ border: '1px solid yellow', width: '335px', display: 'flex', borderRadius: '.3rem', borderWidth: '2px' }}>
+            <SlCalender style={{ margin: '23px 0 13px 10px' }} />
+            <h5 style={{ marginLeft: '2px' }}>Check-in date - Check-out date</h5>
+          </div>
+
+          <div style={{ border: '1px solid yellow', width: '335px', display: 'flex', borderRadius: '.3rem', borderWidth: '2px' }}>
+            <GoPeople style={{ margin: '23px 0 13px 10px' }} />
+            <h5 style={{ marginLeft: '2px' }}>2 adults. 0 children. 1 room</h5>
+          </div>
+
+          <button style={{ width: '145px', backgroundColor: 'rgb(0,62,255)', borderRadius: '.3rem', borderWidth: '2px', border: '1px solid yellow', color: 'white' }}>Search</button>
+        </div>
+      </div>
+
+      <div style={{ margin: '10px auto auto 120px' }}>
+        <input type='checkbox' />
+        <label>I am looking for flights</label>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
