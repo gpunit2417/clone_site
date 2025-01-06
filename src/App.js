d7310e7a593219805ade2c569174455cca8fb4bb
gpunit2417 <punitgoyal106@gmail.com>
2025-01-06 16:24:46 +0530

    removed common navbar rendering

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
-import Navbar from './components/HomePage/Navbar';
 import MainContent from './components/HomePage/MainContent';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ContactPage from './components/ContactPage';
-
+import ListProperty from './components/ListProperty'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App" style={{ margin: '0', padding: '0', top: '0' }}>
       <Router>
-      <Navbar />
         <Routes>
           <Route path='/' Component={MainContent}/>
           <Route path='/contactpage' Component={ContactPage}/>
+          <Route path='/list' Component={ListProperty}/>
         </Routes>
       </Router>
     </div>
