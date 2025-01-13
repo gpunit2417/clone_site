22520b0717d024930f3b39174cad75776ec40697
gpunit2417 <punitgoyal106@gmail.com>
2025-01-13 21:43:26 +0530

    main body completed

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -23,21 +23,21 @@ function RegisterPage() {
                 <p style={{fontSize: '.9rem', margin: '-7px 0 15px 0'}}>You can sign in using your Booking.com account to access our services.</p>
                 <label style={{fontSize: '.9rem', fontWeight: '500'}}>Email address</label><br/>
                 <input type="text" placeholder='Enter your email address' style={{width: '93%', height: '30px', marginTop: '6px', paddingLeft: '6px'}}/><br/>
-                <button style={{margin: '10px 0 18px 0', backgroundColor: 'rgb(13, 110, 253)', color: 'white', border: '1px solid rgb(13, 110, 253)', borderRadius: '5px', height: '7.5vh', width: '25vw', cursor: 'pointer'}}>Continue with email</button>
+                <button style={{margin: '18px 0 18px 0', backgroundColor: 'rgb(13, 110, 253)', color: 'white', border: '1px solid rgb(13, 110, 253)', borderRadius: '5px', height: '7.95vh', width: '25vw', cursor: 'pointer', fontSize: '.95rem', fontWeight: '500'}}>Continue with email</button>
                 <div style={{display: 'flex'}}>
-                    <hr />
-                    <p>or use one of these options</p>
-                    <hr />
+                    <hr style={{height: '0px', width: '75px', margin: 'auto 5px'}}/>
+                    <p style={{margin: '-5px 0 0 0', fontSize: '.9rem'}}>or use one of these options</p>
+                    <hr style={{height: '0px', width: '75px', margin: 'auto 5px'}} />
                 </div>
-                <div style={{display: 'flex'}}>
-                    <div>
-                        <img src="https://cdn2.hubspot.net/hubfs/53/image8-2.jpg" alt="" style={{height: '50px', width: '50px'}}/>
+                <div style={{display: 'flex', justifyContent: 'space-evenly', margin: '30px -1px'}}>
+                    <div style={{border: '1px solid lightgrey', borderRadius: '5px', padding: '5px'}}>
+                        <img src="https://cdn2.hubspot.net/hubfs/53/image8-2.jpg" alt="" style={{height: '30px', width: '50px', marginTop: '10px'}}/>
                     </div>
-                    <div>
-                        <img src="https://www.tailorbrands.com/wp-content/uploads/2021/01/apple_logo_1988.jpg" alt="" style={{height: '50px', width: '50px'}}/>
+                    <div style={{border: '1px solid lightgrey', borderRadius: '5px', padding: '5px'}}>
+                        <img src="https://www.tailorbrands.com/wp-content/uploads/2021/01/apple_logo_1988.jpg" alt="" style={{height: '50px', width: '60px'}}/>
                     </div>
-                    <div>
-                        <img src="https://seekvectors.com/files/download/600dde92dc87c2bf714117db0fa6c8ce.jpg" alt="" style={{height: '50px', width: '50px'}}/>
+                    <div style={{border: '1px solid lightgrey', borderRadius: '5px', padding: '5px'}}>
+                        <img src="https://seekvectors.com/files/download/600dde92dc87c2bf714117db0fa6c8ce.jpg" alt="" style={{height: '48px', width: '50px'}}/>
                     </div>
                 </div>
             </div>
