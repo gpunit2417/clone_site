d8e331bc6d9919c51a50b0f9d8b95abebfbb69f8
gpunit2417 <punitgoyal106@gmail.com>
2025-01-16 18:11:18 +0530

    half mainContent page maintained

--- src/components/HomePage/MainContent.js
+++ src/components/HomePage/MainContent.js
@@ -21,12 +21,15 @@ function MainContent() {
                             <IoBedOutline style={{ margin: '18px 5px 13px 10px' }} />
                             <h3 style={{ margin: '13px 13px 13px 2px', fontWeight: '100', cursor: 'pointer' }}>Stays</h3>
                         </div>
+                        
                         <h3 style={{ margin: '13px 20px', fontWeight: '100', cursor: 'pointer' }}>Flights</h3>
                         <h3 style={{ margin: '13px 20px', fontWeight: '100', cursor: 'pointer' }}>Flight + Hotel</h3>
+                        
                         <div style={{ display: 'flex' }}>
                             <FaCarAlt style={{ margin: '18px 5px 13px 10px', fontWeight: '100' }} />
                             <h3 style={{ margin: '13px 20px 13px 2px', fontWeight: '100', cursor: 'pointer' }}>Car rentals</h3>
                         </div>
+                        
                         <h3 style={{ margin: '13px 20px', fontWeight: '100', cursor: 'pointer' }}>Attractions</h3>
                         <h3 style={{ margin: '13px 20px', fontWeight: '100', cursor: 'pointer' }}>Airport taxis</h3>
                     </div>
@@ -74,14 +77,18 @@ function MainContent() {
                         <div style={{ width: '50%', border: '1px solid grey', margin: '20px 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
                             <h3>Fly away to your dream holiday</h3>
                             <p>Get inspired, compare and book flights with more flexibility</p>
+
                             <img src='https://q-xx.bstatic.com/xdata/images/xphoto/500x500/184698944.png?k=6bb1bf3c13db4a7ba3c22a2d1f1051f793c525a78104703b4dec3eb12101f545&o=' alt='logo' style={{ width: '100px', float: 'right', margin: '-80px 2px 0 auto' }} />
+
                             <button style={{ height: '35px', backgroundColor: 'rgb(0,102,255)', color: 'white', border: 'rgb(0,62,255)', borderRadius: '.3rem', cursor: 'pointer' }}>Search for flights</button>
                         </div>
 
                         <div style={{ width: '50%', border: '1px solid grey', margin: '20px 0 10px 5px', borderRadius: '.4rem', padding: '5px 7px' }}>
                             <h3>Go for a good time, not a long time</h3>
                             <p>Finish your year with a mini break. Save 15% or more <br />when you book and stay by 7 January 2025.</p>
+
                             <img src='https://q-xx.bstatic.com/xdata/images/xphoto/500x500/372051085.jpeg?k=f5182852fa2c998e2b47cfd922da41ae16c42beb02fb70ecb57ee55ced3271f6&o=' alt='logo' style={{ width: '130px', float: 'right', margin: '-108px 10px auto auto' }} />
+
                             <button style={{ height: '35px', backgroundColor: 'rgb(0,102,255)', color: 'white', border: 'rgb(0,62,255)', borderRadius: '.3rem', cursor: 'pointer' }}>Find late escape deals</button>
                         </div>
                     </div>
@@ -95,13 +102,18 @@ function MainContent() {
                         <h1>Trending Destinations</h1>
                         <p style={{ fontSize: '16px', margin: '-15px auto auto auto' }}>Most popular choices for travellers from India</p>
                     </div>
+
                     <div style={{ display: 'flex', width: '90%' }}>
+                        
                         <div style={{ width: '50%', margin: '0 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
                             <h2 style={{ top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white', textShadow: ' 1px 1px black' }}>New Delhi <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1J4X8LJ0h28DpeUykXQcMiNAXXWLhvhW0g&s" alt="flag" style={{ height: '15px', width: '25px', margin: '26px 0 0 4px', objectFit: 'cover' }} /></h2>
+
                             <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684765.jpg?k=3f7d20034c13ac7686520ac1ccf1621337a1e59860abfd9cbd96f8d66b4fc138&o=" alt="trending" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover', height: '50vh', cursor: 'pointer' }} />
                         </div>
+
                         <div style={{ width: '50%', margin: '0 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
                             <h2 style={{ top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white', textShadow: ' 1px 1px black' }}>Bangalore <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1J4X8LJ0h28DpeUykXQcMiNAXXWLhvhW0g&s" alt="flag" style={{ height: '15px', width: '25px', margin: '26px 0 0 4px', objectFit: 'cover' }} /></h2>
+
                             <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684534.jpg?k=d1fe86c22f2433f4e2dda14ddcbe80feb024b0fb30305e5684a1241fba5d4cff&o=" alt="location" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover', height: '50vh', cursor: 'pointer' }} />
                         </div>
                     </div>
@@ -109,14 +121,19 @@ function MainContent() {
                     <div style={{ display: 'flex', width: '90%', margin: '-15px 0 0 0' }}>
                         <div style={{ width: '33%', margin: '0 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
                             <h2 style={{ top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white', textShadow: ' 1px 1px black' }}>Mumbai <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1J4X8LJ0h28DpeUykXQcMiNAXXWLhvhW0g&s" alt="flag" style={{ height: '15px', width: '25px', margin: '26px 0 0 4px', objectFit: 'cover' }} /></h2>
+                            
                             <img src="https://cf2.bstatic.com/xdata/images/city/600x600/971346.jpg?k=40eeb583a755f2835f4dcb6900cdeba2a46dc9d50e64f2aa04206f5f6fce5671&o=" alt="trending" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover', height: '40vh', cursor: 'pointer' }} />
                         </div>
+                        
                         <div style={{ width: '33%', margin: '0 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
                             <h2 style={{ top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white', textShadow: ' 1px 1px black' }}>Chennai <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1J4X8LJ0h28DpeUykXQcMiNAXXWLhvhW0g&s" alt="flag" style={{ height: '15px', width: '25px', margin: '26px 0 0 4px', objectFit: 'cover' }} /></h2>
+                            
                             <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684730.jpg?k=e37b93d88c1fe12e827f10c9d6909a1def7349be2c68df5de885deaa4bc01ee3&o=" alt="trending" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover', height: '40vh', cursor: 'pointer' }} />
                         </div>
+                        
                         <div style={{ width: '33%', margin: '0 5px 10px 0', borderRadius: '.4rem', padding: '0 7px' }}>
                             <h2 style={{ top: '54px', position: 'relative', margin: '-31px 0 0 25px', color: 'white', textShadow: ' 1px 1px black' }}>Hyderabad <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1J4X8LJ0h28DpeUykXQcMiNAXXWLhvhW0g&s" alt="flag" style={{ height: '15px', width: '25px', margin: '26px 0 0 4px', objectFit: 'cover' }} /></h2>
+                            
                             <img src="https://cf2.bstatic.com/xdata/images/city/600x600/684653.jpg?k=306ccafcc8a4a7e23b9e8a05b183453fe885b312a4daa5ce76ec39a1b79cbc6f&o=" alt="location" style={{ borderRadius: '.5rem', width: '100%', objectFit: 'cover', height: '40vh', cursor: 'pointer' }} />
                         </div>
                     </div>
